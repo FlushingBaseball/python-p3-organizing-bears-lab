077506532a60eb8596202ecfa6942d35ba50b949
Sam Peck <fullstacksam@protonmail.com>
2023-07-08 22:44:23 -0400

    going back yall

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 4, "M", "pink", "angrey", True);
+INSERT INTO bears ( name, age, sex, color, temperament, alive) VALUES ("Rowdy", 4, "M", "pink", "angrey", True);
+INSERT INTO bears ( name, age, sex, color, temperament, alive) VALUES ("Tabitha", 3, "M", "pink", "angrey", True);
+INSERT INTO bears ( name, age, sex, color, temperament, alive) VALUES ("Sergeant Brow", 1,"M", "pink", "angrey", True);
+INSERT INTO bears ( name, age, sex, color, temperament, alive) VALUES ("Melissa", 44, "M", "pink", "angrey", True);
+INSERT INTO bears ( name, age, sex, color, temperament, alive) VALUES ("Grinch", 34,"M", "pink", "angrey", True);
+INSERT INTO bears ( name, age, sex, color, temperament, alive) VALUES ("Wendy", 14, "M", "pink", "angrey", True);
+INSERT INTO bears ( name, age, sex, color, temperament, alive) VALUES (NULL, 25, "M", "pink", "angrey", True);
